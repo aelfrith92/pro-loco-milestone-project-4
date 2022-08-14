a2198e346c14eae2f1009986ad003b8ffe930c45
Alfredo Morieri <fred.morieri@gmail.com>
2022-08-14 15:49:03 +0000

    Add clarity via docstring and comments in models.py

--- eventsblog/models.py
+++ eventsblog/models.py
@@ -2,12 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 from django.utils import timezone
-from datetime import datetime, timedelta
+# datetime rimossa dalla seguente importazione
+from datetime import timedelta
 
 STATUS = ((0, "Draft"), (1, "Scheduled"), (2, "Cancelled"))
 AUDIENCE = ((0, "Admin"), (1, "Everyone"))
 
+
 class Event(models.Model):
+    '''This class defines the Event model'''
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
@@ -21,10 +24,15 @@ class Event(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     joins = models.ManyToManyField(
         User, related_name='blogevent_like', blank=True)
+    # The next field sets the date +14 days at the time of creation
+    # to give enugh time to admins to approve and organize the event
     scheduled_on = models.DateTimeField(default=timezone.now() +
                                         timedelta(days=14))
 
     class Meta:
+        '''The following line defines the ascending order that data is
+           retrieved by, based on the field scheduled_on:
+           the closer the date, the higher in the page'''
         ordering = ["scheduled_on"]
 
     def __str__(self):
@@ -35,6 +43,7 @@ class Event(models.Model):
 
 
 class Comment(models.Model):
+    '''This class defines the comment model'''
     event = models.ForeignKey(Event, on_delete=models.CASCADE,
                               related_name="events")
     name = models.CharField(max_length=50)
