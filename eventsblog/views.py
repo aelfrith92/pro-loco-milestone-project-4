b68b2e50871b857e3bf0260e4e56e7a7639f89be
Alfredo Morieri <fred.morieri@gmail.com>
2022-08-20 22:55:13 +0000

    POST method for comments, add leave comments functionality, let jinja distinguish between admins and normal users to determine approvals

--- eventsblog/views.py
+++ eventsblog/views.py
@@ -33,3 +33,36 @@ class EventOverview(View):
                 "comment_form": CommentEventForm(),
             },
         )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Event.objects.filter(status=1)
+        event = get_object_or_404(queryset, slug=slug)
+        comments = event.comments.filter(approved=True).order_by("-created_on")
+        joined = False
+        # if event.joins.filter(id=self.request.user.id).exists():
+        #     joined = True
+
+        comment_form = CommentEventForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            if request.user.is_staff:
+                comment_form.instance.approved = True
+            comment = comment_form.save(commit=False)
+            comment.event = event
+            comment.save()
+        else:
+            comment_form = CommentEventForm()
+        
+        return render(
+            request,
+            "event_overview.html",
+            {
+                "event": event,
+                "comments": comments,
+                "commented": True,
+                "joined": joined,
+                "comment_form": CommentEventForm(),
+            },
+        )
