70ee5227bfdde1f85d2d199d94535cd344834c23
Alfredo Morieri <fred.morieri@gmail.com>
2022-08-21 18:09:26 +0000

    Fix deploy issue with style 'MIME type ('text/html') is not a supported stylesheet MIME type, and strict MIME checking is enabled', fix settings.py, fix env vars in Heroku, attempt to fix style within header and unwanted top margin of banner

--- prolocosupersano/settings.py
+++ prolocosupersano/settings.py
@@ -165,7 +165,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.' \
                       'StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
@@ -173,6 +173,5 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-# https://res.cloudinary.com/aelfrith/raw/upload/v1661079981/static/css/style.css
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
