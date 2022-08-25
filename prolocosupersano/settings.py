90bd52c93fa5ee9d3ad4e154beeb0e21045376ae
Alfredo Morieri <fred.morieri@gmail.com>
2022-08-25 22:20:39 +0000

    Add summernote config

--- prolocosupersano/settings.py
+++ prolocosupersano/settings.py
@@ -112,17 +112,17 @@ WSGI_APPLICATION = 'prolocosupersano.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if development:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
+# if development:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+# else:
+DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
+}
 
 
 # Password validation
